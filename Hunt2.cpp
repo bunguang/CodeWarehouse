b57336ca7a22b0ccc3d146c58aa0eccb68adb8de
Wenguang Huang <blueskygundam@sjtu.edu.cn>
2018-03-20 23:57:57 +0800

    update the 2rd problem of the interview

--- Hunt2.cpp
+++ Hunt2.cpp
@@ -6,11 +6,7 @@
 #include <string>
 using namespace std;
 
-char partitionExpression(string str) {
-    if (str.size() > 5) char result = partitionExpression(str);
-}
-
-char expressionResult(string str) {
+string expressionResult(string str) {
     if (str.size() > 5) {
         string sub = str.substr(1, str.size() - 2);
         int count = 0;
@@ -22,43 +18,43 @@ char expressionResult(string str) {
             char tmpChar = sub[i];
             if (tmpChar == '(') count++;
             if (tmpChar == ')') count--;
-
             if (count == 0 && tmpChar == '!') {
                 sub_f = sub.substr(i + 1);
-                char tmp_str_f = expressionResult(sub_f);
-                t_str = "(" + to_string(tmpChar) + to_string(tmp_str_f) + ")";
+                string tmp_str_f = expressionResult(sub_f);
+                t_str = "(";
+                t_str.insert(t_str.end(), tmpChar);
+                t_str = t_str + tmp_str_f + ")";
                 return expressionResult(t_str);
-                break;
             }
-
             if (count == 0 && (tmpChar == '|' || tmpChar == '&')) {
                 sub_l = sub.substr(0, i);
                 sub_r = sub.substr(i + 1);
-                char tmp_str_l = expressionResult(sub_l);
-                char tmp_str_r = expressionResult(sub_r);
-                str = "(" + to_string(tmp_str_l) + to_string(tmpChar)+ to_string(tmp_str_r) + ")";
+                string tmp_str_l = expressionResult(sub_l);
+                string tmp_str_r = expressionResult(sub_r);
+                t_str = "(" + tmp_str_l;
+                t_str.insert(t_str.end(), tmpChar);
+                t_str = t_str + tmp_str_r + ")";
                 return expressionResult(t_str);
-                break;
             }
         }
     }
 
-    if (str.size() == 1) return str[0];
+    if (str.size() == 1) return str;
     if (str.size() == 4) {
-        if (str[2] == '0') return '1';
-        else if (str[2] == '1') return '0';
+        if (str[2] == '0') return "1";
+        else if (str[2] == '1') return "0";
     }
     if (str.size() == 5) {
         if (str[2] == '|') {
-            if (str[1] == '1' || str[3] == '1') return '1';
-            else return '0';
+            if (str[1] == '1' || str[3] == '1') return "1";
+            else return "0";
         } else if (str[2] == '&') {
-            if (str[1] == '0' || str[3] == '0') return '0';
-            else return '1';
+            if (str[1] == '0' || str[3] == '0') return "0";
+            else return "1";
         }
     }
 
-    return '1';
+
 }
 
 int main() {
@@ -68,7 +64,7 @@ int main() {
         string tmpStr;
         cin >> tmpStr;
         // string sub = tmpStr.substr(1, tmpStr.size() - 1);
-        char result = expressionResult(tmpStr);
+        string result = expressionResult(tmpStr);
         cout << result << endl;
         // 这里可能要考虑释放一下string变量的值, 内存有限制
     }
